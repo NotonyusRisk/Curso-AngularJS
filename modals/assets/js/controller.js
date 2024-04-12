a9a3260f9700b2d13f9e7d64dc18d6dcd4091ffe
Antonio <antoniojrudass@gmail.com>
2024-04-12 15:57:52 -0500

    feat::funcionalidad de operadores y botones

--- modals/assets/js/controller.js
+++ modals/assets/js/controller.js
@@ -1,5 +1,45 @@
 let app = angular.module('mainModule', [])
 
 app.controller('firstController', function ($scope, $http) {
-    $scope.numbers = []
+    $scope.display = '0';
+    $scope.primeraParte = '';
+    $scope.operador = null;
+    $scope.segundaParte = '';
+
+    $scope.agregarNumero = (numero) => {
+        if($scope.display === '0'){
+            $scope.display = '';
+        }
+        $scope.display += numero;
+    }
+
+    $scope.agregarOperador = (operador) => {
+        if ($scope.display !== ''){
+            $scope.primeraParte = $scope.display;
+            $scope.operador = operador;
+            $scope.display = ''
+        }
+    }
+
+    $scope.resultado = () => {
+        if($scope.operador !== null && $scope.display !== ''){
+            $scope.segundaParte = $scope.display;
+        }
+        
+        let resultado = 0;
+        switch($scope.operador){
+            case '+':
+                resultado = parseFloat($scope.primeraParte) + parseFloat($scope.segundaParte);
+                break;
+            case '-':
+                resultado = parseFloat($scope.primeraParte) - parseFloat($scope.segundaParte);
+                break;
+            case '*':
+                resultado = parseFloat($scope.primeraParte) * parseFloat($scope.segundaParte);
+                break;
+            case '/':
+                resultado = parseFloat($scope.primeraParte) / parseFloat($scope.segundaParte);
+                break;
+        }
+    }
 })
