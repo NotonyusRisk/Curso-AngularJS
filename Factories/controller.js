16f9ca1c9ac6f0f9216f6d7501b202577958b5b7
Antonio <antoniojrudass@gmail.com>
2024-04-13 18:57:44 -0500

    feat::controller.js

--- Factories/controller.js
+++ Factories/controller.js
@@ -3,7 +3,9 @@ let app = angular.module('mainModule', []);
 app.controller('firstController', function($scope) {
     $scope.todo = [];
     $scope.newActv = {};
+
     $scope.addActv = () => {
         $scope.todo.push($scope.newActv);
+        console.log($scope.todo)
     }
 });
